0f34e32b7c0b6d8eacfae13799cfce9df80bdae8
anuragkothare <anuragkothare@outlook.com>
2023-06-24 19:40:57 +0530

    Solved subset sum eaual to k

--- Subset_Sum_K/main.go
+++ Subset_Sum_K/main.go
@@ -8,59 +8,30 @@ func main() {
 	arr := []int{1, 7, 2, 9, 10}
 	n := len(arr)
 	k := 6
-	res := subsetSumToK2(n, k, &arr)
+	res := subsetSumToK(n, k, &arr)
 	fmt.Println("Subset sum to", k, "exists:", res)
 }
 
 // Tabulation
 func subsetSumToK(n int, k int, arr *[]int) bool {
-	dp := make([][]bool, n)
+	// dp[i][j] will represent whether it is possible to achieve the sum j using the first i elements of the nums array.
+	dp := make([][]bool, n+1)
 	for i := range dp {
 		dp[i] = make([]bool, k+1)
 	}
 
-	for i := 0; i < n; i++ {
+	for i := 0; i <= n; i++ {
 		dp[i][0] = true
-		dp[0][(*arr)[0]] = true
+	}
 
-		for ind := 1; ind < n; ind++ {
-			for target := 1; target <= k; target++ {
-				notTake := dp[ind-1][target]
-				take := false
-
-				if (*arr)[ind] <= target {
-					take = dp[ind-1][target-(*arr)[ind]]
-				}
-				dp[ind][target] = take || notTake
-
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= k; j++ {
+			if (*arr)[i-1] > k {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j] || dp[i-1][j-(*arr)[i-1]]
 			}
 		}
 	}
-	return dp[n-1][k]
-}
-
-// Space Optimization of Tabulation
-func subsetSumToK2(n int, k int, arr *[]int) bool {
-	fmt.Println(*arr)
-	prev := make([]bool, k+1)
-	curr := make([]bool, k+1)
-
-	prev[0] = true
-	curr[0] = true
-	prev[(*arr)[0]] = true
-
-	for ind := 1; ind < n; ind++ {
-		for target := 1; target <= k; target++ {
-			notTake := prev[target]
-			take := false
-
-			if (*arr)[ind] <= target {
-				take = prev[target-(*arr)[ind]]
-			}
-			curr[target] = take || notTake
-		}
-		prev = curr
-	}
-
-	return prev[k]
+	return dp[n][k]
 }
